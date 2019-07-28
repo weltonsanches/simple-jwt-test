d8448e8d2bcd777f305095ab276e5c7e20ae807c
Welton Sanches <sanches.welton@gmail.com>
2019-07-28 17:13:02 -0300

    adding token endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 
+require("dotenv").config();
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+
+const createUserToken = (user, password) => {
+  return jwt.sign({ id: `${user}${password}` }, process.env.JWT_PASS, { expiresIn: process.env.EXPIRES_IN });
+}
 
 router
-  .get('/', (req, res) => {
-    const message = {};
-    res.send(message);
+  .post('/token', (req, res) => {
+    const { user, password } = req.body;
+    if (!user || !password)
+      return res.status(403).send({ error: 'Dados insuficientes!' });
+
+    if (user !== 'Numenu' || password !== '123123')
+      return res.status(403).send({ error: 'Credenciais incorretas!' });
+
+    return res.send({ token: createUserToken(user, password) });
   });
 
 module.exports = router;
